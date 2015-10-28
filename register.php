ca5bc10a0c31da8f0f971ffdfd13a09bffb89563
klendo <klendo@klendo.pgs-soft.com>
2015-10-28 15:37:32 +0100

    visual improvement

--- register.php
+++ register.php
@@ -2,19 +2,32 @@
 
 error_reporting(-1);
 
-echo '<pre>', print_r(array(
-    'get' => $_GET,
-    'post' => $_POST), true), '</pre>';
-
 # Get values of variables
 $username = $_POST['username'];
 $password = $_POST['password'];
-$retype = $_POST['retype']
+$retype = $_POST['retype'];
 
 # Send data to database
+if ($username == '' or $password == '' or $retype == '') {
+    echo 'All fields must be filled in order to register <br/>';
+    exit();
+}
+if ($password !== $retype) {
+    echo 'Provided passwords have to be identical <br/>';
+    exit();
+}
+$database = new PDO('mysql:host=localhost;dbname=userdata', 'root', '');
+//$database->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$user_existance = "SELECT COUNT(*) AS count FROM users WHERE Username = :username";
+$prepare_user_existance_querry = $database->prepare($user_existance);
+$prepare_user_existance_querry->execute(array(':username' => $username));
+$existance = $prepare_user_existance_querry->fetchObject()->count;
+
+if ($existance != 0) {
+    echo 'You are known here, no need to introduce again. <br/>';
+    exit();
+}
 try {
-    $database = new PDO('mysql:host=localhost;dbname=userdata', 'root', '');
-    $database->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $adduser = "INSERT INTO users (Username, Password) VALUES (:username,:password)";
     $prepare_add_user_querry = $database->prepare($adduser);
     $prepare_add_user_querry->execute(array(':username' => $username, ':password' => md5($password)));
@@ -27,4 +40,7 @@ print_r("<br/>");
 var_dump($password);
 print_r("<br/>");
 var_dump($database);
-?>
+
+echo '<pre>', print_r(array(
+    'get' => $_GET,
+    'post' => $_POST), true), '</pre>';
